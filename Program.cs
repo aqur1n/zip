3a221b408b714dc1ebb40af0edc262664d23cff8
aqur1n <60589309+aqur1n@users.noreply.github.com>
2021-12-24 16:00:53 +0700

    add function for copy folder...

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace zip
         {
             string Temp = Path.GetTempPath() + @"/aqur1n-zip";
             string[] Files;
-
+            
             if (args.Length == 1 && Path.GetFileName(args[0]) == "zip.txt")
             {
                 Files = File.ReadAllLines(args[0]).ToArray();
@@ -22,23 +22,51 @@ namespace zip
             {
                 Files = args;
             }
-
+            
             if (Directory.Exists(Temp))
             {
                 Directory.Delete(Temp, true);
             }
 
             Directory.CreateDirectory(Temp);
+
+            string NameZip = "ZepZip";
             foreach (string File in Files)
             {
-                System.IO.File.Copy(File, Temp + "/" + Path.GetFileName(File));
+                if (System.IO.File.Exists(File))
+                {
+                    System.IO.File.Copy(File, Path.Combine(Temp, Path.GetFileName(File)));
+
+                    NameZip = Path.GetFileName(File);
+                } else if (Directory.Exists(File.Split(" => ")[0]))
+                {
+                    string[] _File = File.Split(" => ");
+
+                    Directory.CreateDirectory(Path.Combine(Temp, _File[1]));
+                    DirectoryCopy(_File[0], Path.Combine(Temp, _File[1]));
+                }
             }
 
-            if (File.Exists(Directory.GetCurrentDirectory() + "/zip.zip"))
+            if (File.Exists(Directory.GetCurrentDirectory() + "/" + NameZip + ".zip"))
             {
-                File.Delete(Directory.GetCurrentDirectory() + "/zip.zip");            }
-            ZipFile.CreateFromDirectory(Temp, Directory.GetCurrentDirectory() + "/zip.zip");
+                File.Delete(Directory.GetCurrentDirectory() + "/" + NameZip + ".zip");            
+            }
+            ZipFile.CreateFromDirectory(Temp, Directory.GetCurrentDirectory() + "/" + NameZip + ".zip");
             Directory.Delete(Temp, true);
+
+            Environment.Exit(0);
+        }
+
+        private static void DirectoryCopy(string SourcePath, string DestinationPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+            }
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+            }
         }
     }
 }
